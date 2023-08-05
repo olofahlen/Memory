1ac02f6bcdd769b52b3fe5c4a38a84a737371eb7
Olof Ahlén <olof.ahlen@gmail.com>
2023-08-05 16:22:58 +0100

    Initial commit.

---
+++ scraper.py
@@ -0,0 +1,42 @@
+import bs4 as bs
+import urllib.request
+import pandas as pd
+import typing
+import os
+import pathlib
+import logging
+import time
+
+NAMES = typing.Literal["Verb", "Adjektiv", "Substantiv"]
+
+logging.basicConfig(level=logging.INFO)
+
+
+def scrape_page(name: NAMES, page: int) -> None:
+    folder = pathlib.Path(name)
+    os.makedirs(folder, exist_ok=True)
+    url = f"https://doon.se/ordtyp/{name}?page={page}"
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, "lxml")
+    words = pd.Series([x.string for x in soup.find_all("a", attrs={"class": "slider"})])
+    if words.empty:
+        raise EOFError(f"No words on this page. {url = }")
+    words.to_csv(folder / f"{page}.csv", index=False, header=False)
+
+
+def main() -> None:
+    for name in typing.get_args(NAMES):
+        page = 1
+        while True:
+            logging.info(f"Scraping {name} page {page}")
+            try:
+                scrape_page(name, page)
+            except EOFError:
+                logging.info(f"Reached the end. Exiting.")
+                break
+            page += 1
+            time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
